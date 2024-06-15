8a1933ebb45714bf834857f5d50718c21f6ad1be
Daffa Ilhami <mdaffailhami@gmail.com>
2024-06-15 20:54:02 +0800

    Update e_ticket.dart

--- lib/pages/e_ticket.dart
+++ lib/pages/e_ticket.dart
@@ -6,10 +6,11 @@ class MyETicketPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 75, horizontal: 25),
+    return Center(
       child: Container(
-        padding: EdgeInsets.all(25),
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: 620,
+        padding: EdgeInsets.fromLTRB(25, 0, 25, 25),
         decoration: BoxDecoration(
           border: Border.all(
             color: Theme.of(context).colorScheme.primary,
@@ -17,76 +18,97 @@ class MyETicketPage extends StatelessWidget {
           ),
           borderRadius: BorderRadius.circular(12),
         ),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primary,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 37.5,
-                backgroundColor: Colors.white,
-                child: CircleAvatar(
-                  foregroundImage: NetworkImage(
-                    'https://avatars.githubusercontent.com/u/74972129?v=4',
-                  ),
-                  radius: 36,
-                ),
-              ),
-              Text(
-                'Muhammad Daffa Ilhami',
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    fontWeight: FontWeight.bold, color: Colors.white),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.tag,
-                    size: 20,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    '612896192837',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                          color: Colors.white,
-                        ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              QrImageView(
-                data: '1234567890',
-                version: QrVersions.auto,
-                size: MediaQuery.of(context).size.width * 0.55,
-                backgroundColor: Colors.white,
-              ),
-              SizedBox(height: 15),
-              Text(
-                'Nomor E-Ticket',
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Text(
+                'E-Ticket\nLPG Bersubsidi',
+                textAlign: TextAlign.center,
                 style: Theme.of(context)
                     .textTheme
-                    .headlineSmall
-                    ?.copyWith(color: Colors.white),
+                    .headlineLarge
+                    ?.copyWith(fontWeight: FontWeight.bold),
               ),
-              Text(
-                'LPG-2024APR02',
-                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                    fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 37.5,
+                      backgroundColor: Colors.white,
+                      child: CircleAvatar(
+                        foregroundImage: NetworkImage(
+                          'https://avatars.githubusercontent.com/u/74972129?v=4',
+                        ),
+                        radius: 36,
+                      ),
+                    ),
+                    Text(
+                      'Muhammad Daffa Ilhami',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.tag,
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          '612896192837',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style:
+                              Theme.of(context).textTheme.labelLarge?.copyWith(
+                                    color: Colors.white,
+                                  ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    QrImageView(
+                      data: '1234567890',
+                      version: QrVersions.auto,
+                      size: MediaQuery.of(context).size.width * 0.55,
+                      backgroundColor: Colors.white,
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      'Nomor E-Ticket',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall
+                          ?.copyWith(color: Colors.white),
+                    ),
+                    Text(
+                      'LPG-2024APR02',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium
+                          ?.copyWith(
+                              fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                    Text(
+                      'Berlaku sampai 25/04/2024',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
               ),
-              Text(
-                'Berlaku sampai 25/04/2024',
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
